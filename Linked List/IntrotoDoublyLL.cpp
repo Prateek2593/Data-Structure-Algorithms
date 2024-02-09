8b2b3c79277a1a6a5ab640ede1a6320f40176fd5
Prateek Patidar <125291511+Prateek2593@users.noreply.github.com>
2024-02-09 22:30:35 +0530

    Add files via upload

---
+++ Linked List/IntrotoDoublyLL.cpp
@@ -0,0 +1,12 @@
+Node* constructDLL(vector<int>& arr) {
+    int n = arr.size();
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i=1;i<n;i++){
+        Node* temp = new Node(arr[i],nullptr,prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
