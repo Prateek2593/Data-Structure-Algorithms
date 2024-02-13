de8512c07c5f4088b5accbcd6654de61dd835d2a
Prateek Patidar <125291511+Prateek2593@users.noreply.github.com>
2024-02-13 19:51:45 +0530

    Add files via upload

---
+++ Linked List/ReverseSingleLL.cpp
@@ -0,0 +1,8 @@
+ if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    Node* newHead = reverseLinkedList(head->next);
+    Node* front = head->next;
+    front->next = head;
+    head->next = NULL;
+    return newHead;
