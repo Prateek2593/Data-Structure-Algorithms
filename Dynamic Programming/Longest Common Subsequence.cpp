6ed95d76cb520e3c03fe06914a86deb21a05265d
Prateek Patidar <125291511+Prateek2593@users.noreply.github.com>
2024-06-05 23:15:37 +0530

    Update Longest Common Subsequence.cpp

--- Dynamic Programming/Longest Common Subsequence.cpp
+++ Dynamic Programming/Longest Common Subsequence.cpp
@@ -25,3 +25,29 @@ public:
         return LCS(text1, text2, n, m);
     }
 };
+
+Solution 2: Recursion + Memoization
+class Solution {
+public:
+    int t[1001][1001];
+    int LCS(string &text1, string &text2, int n, int m){
+        if(n == 0 || m == 0){
+            return 0;
+        }
+        if(t[n][m] != -1){
+            return t[n][m];
+        }
+        if(text1[n-1] == text2[m-1]){
+            return t[n][m] = 1+ LCS(text1, text2, n-1, m-1);
+        }
+        else{
+            return t[n][m] = max(LCS(text1, text2, n-1, m), LCS(text1, text2, n, m-1));
+        }
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length();
+        int m = text2.length();
+        memset(t, -1, sizeof(t));
+        return LCS(text1, text2, n, m);
+    }
+};
